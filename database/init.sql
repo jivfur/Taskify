5497baaed685323501e8a65d2b82e611a91936e9
Ivan Fuentes <jivfur@gmail.com>
2024-11-25 23:49:46 -0500

    Update database schema: Add UNIQUE constraint and change 'complete' column to INTEGER

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS tasks (
     taskId INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(255),
     description TEXT,
     deadline INTEGER,  -- You can store timestamps
     exitCriteria TEXT,
-    complete BOOLEAN
+    complete INTEGER,   -- Use INTEGER to represent BOOLEAN (0 for false, 1 for true)
+    UNIQUE (title, deadline, description, exitCriteria)  -- Composite unique index
 );
-
